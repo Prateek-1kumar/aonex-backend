da75b29368a4950200614d434cdf000adf78b967
Prateek Kumar <prateek45698@gmail.com>
2026-05-15 04:10:03 +0530

    feat(lib-utils): unit conversion utility

--- packages/lib-utils/src/index.ts
+++ packages/lib-utils/src/index.ts
@@ -4,3 +4,4 @@ export * from "./remove-nango-metadata.js";
 export * from "./clock.js";
 export * from "./backoff.js";
 export * from "./url.js";
+export { convertToCanonical, canonicalUnitFor, type Dimension } from "./units.js";
