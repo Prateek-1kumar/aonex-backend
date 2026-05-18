ae523b8e0a2123183ac944e29093ddbde7d14f72
Prateek Kumar <prateek45698@gmail.com>
2026-05-18 16:19:44 +0530

    feat(enrichment): image role classifier (hero/gallery/swatch/lifestyle/spec/video_thumb)

--- packages/ingestion/enrichment/src/index.ts
+++ packages/ingestion/enrichment/src/index.ts
@@ -2,3 +2,4 @@
 // Filled in by Tasks 3.2-3.7.
 
 export { normalizeImageUrls, type RawImg, type NormalizedImg } from "./image-normalizer.js";
+export { classifyImageRoles, type ImageRole, type RoledImg } from "./image-role-classifier.js";
