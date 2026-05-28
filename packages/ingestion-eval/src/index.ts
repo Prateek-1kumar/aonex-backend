9ba21d300039edac020225a652cf50df8bf74e7c
Prateek Kumar <prateek45698@gmail.com>
2026-05-28 22:19:08 +0530

    feat(eval): public exports

--- packages/ingestion-eval/src/index.ts
+++ packages/ingestion-eval/src/index.ts
@@ -1,1 +1,9 @@
 export const EVAL_PACKAGE = "@aonex/ingestion-eval";
+export * from "./types.js";
+export { fieldWeight, CRITICAL_FIELDS } from "./field-weights.js";
+export { fieldsMatch } from "./normalize.js";
+export { scoreProduct, aggregate } from "./score-extraction.js";
+export { promoteMetrics, type PromoteMetrics } from "./promote-metrics.js";
+export { loadGoldenSet, splitBy } from "./golden-set.js";
+export { toLabeledSamples } from "./calibration-bridge.js";
+export { evaluateGate, DEFAULT_THRESHOLDS, type GateInput, type GateResult } from "./gate.js";
