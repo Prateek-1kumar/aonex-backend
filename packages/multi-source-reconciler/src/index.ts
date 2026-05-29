44d7640e06b4cea0f83486e2b209fd0f0847f27b
Prateek Kumar <prateek45698@gmail.com>
2026-05-29 07:42:12 +0530

    test(er): blocking+multi-feature ER beats jaro-winkler baseline

--- packages/multi-source-reconciler/src/index.ts
+++ packages/multi-source-reconciler/src/index.ts
@@ -13,3 +13,5 @@ export {
   type ReconciliationAction,
   type ReconciliationDecision
 } from "./policy.js";
+export { blockingKeys, type BlockingSignals } from "./blocking.js";
+export { scorePair, type PairSide, type PairScore } from "./pair-features.js";
