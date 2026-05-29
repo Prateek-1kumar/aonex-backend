e30290c11c8968ad08ce710f6a9bae3855ccca49
Prateek Kumar <prateek45698@gmail.com>
2026-05-29 06:02:08 +0530

    feat(backfill): multi-candidate GTIN lookup with cost cap

--- packages/gtin-backfill/src/index.ts
+++ packages/gtin-backfill/src/index.ts
@@ -1,2 +1,9 @@
 export const GTIN_BACKFILL_PACKAGE = "@aonex/gtin-backfill";
 export { roundTripMatches } from "./round-trip.js";
+export {
+  backfillGtin,
+  type RegistryHit,
+  type BackfilledIdentifier,
+  type BackfillDeps,
+  type BackfillResult,
+} from "./lookup.js";
